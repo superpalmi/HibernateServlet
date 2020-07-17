a9dc3138b09a1a85240cbc94a1eb31b39e213e56
riccardopalmieri95 <68059318+riccardopalmieri95@users.noreply.github.com>
2020-07-17 15:19:12 +0200

    Added Maven
    Removed old jars

---
+++ src/main/java/com/palmieri/hibernate/model/User.java
@@ -0,0 +1,116 @@
+package com.palmieri.hibernate.model;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name="username", nullable = false)
+    private String userName;
+
+    @Column(name="password", nullable = false)
+    private String password1;
+    @Column(name="email", nullable = false)
+    private String email;
+    @Column(name="phone", nullable = false)
+    private String phone;
+    @Column(name="city", nullable = false)
+    private String city;
+
+
+
+    @Column(name="role", nullable = true)
+    private String role;
+
+
+
+
+    @OneToMany (cascade = {CascadeType.ALL})
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Reservation> reservations;
+
+
+
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword1() {
+        return password1;
+    }
+
+    public void setPassword1(String password1) {
+        this.password1 = password1;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations( Reservation reservation) {
+        this.reservations.add(reservation) ;
+    }
+
+
+
+}
